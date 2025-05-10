df771655ebbe4e898e0344743be1831981654f03
Devesh Chandra <deveshru2712@gmail.com>
2025-05-10 08:00:12 +0530

    added the server folder to workspace and created a simple http server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { isHttpError } from "http-errors";
+import authRouter from "./routes/auth.routes";
+import env from "./utils/validateEnv";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hii there i am yash chandra");
+});
+
+app.use("/api/auth", authRouter);
+// app.use("/api/message", messageRouter);
+
+app.use((req, res, next) => {
+  next(createHttpError(404, "Page not found!"));
+});
+
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  let errMessage = err || "Unknown error occurred";
+  let statusCode = 500;
+  if (isHttpError(err)) {
+    statusCode = err.statusCode;
+    errMessage = err.message;
+  }
+
+  res.status(statusCode).json({ error: errMessage });
+});
+
+app.listen(env.PORT, () => {
+  console.log(`Server is running on the port:${env.PORT}`);
+});
